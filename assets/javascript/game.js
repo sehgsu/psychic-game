c1955b25846bd14bb35d894d550e667b64be50a3
Alex <alex@Alex.local>
2019-03-13 00:09:56 -0400

    reset + psuedo'd

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// psuedo code first
+// set player and computer variables
+// computerChoice
+// key up event userGuess
+// determine if correct guess
+// iff correct players gets 1 win 
+// else player loses 1 life loop 9 times until lose
+
+
