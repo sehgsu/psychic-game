01317f9fdd0d86da93b8e4c0ba6b5c851ed6087b
Alex <alex@Alex.local>
2019-03-14 17:39:31 -0400

    updating java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,42 @@
-// psuedo code first
-// set player and computer variables
-// computerChoice
-// key up event userGuess
-// determine if correct guess
-// iff correct players gets 1 win 
-// else player loses 1 life loop 9 times until lose
-
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-"r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var victories = 0;
 var losses = 0;
 var livesLeft = 9;
 var lettersUsed = [];
 
-var userGuessText = document.getElementById("userguess-text");
-var livesLeftText = document.getElementById("livesleft-text");
-var computerPickText = document.getElementById("computerpick-text");
-var victoryText = document.getElementById("victory-text");
-var loseText = document.getElementById("lose-text");
+ var userGuessText = document.getElementById("userguess-text");
+ var livesLeftText = document.getElementById("livesleft-text");
+ var victoryText = document.getElementById("victory-text");
+ var loseText = document.getElementById("lose-text");
 
+ var reset = function () {
+     livLeft = 9;
+     guessedLetters = [];
+    
+}
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 document.onkeyup = function(event) {
+    livesLeft--;
+        document.getElementById("livesleft-text").innerHTML = livesLeft
     var userGuess = event.key;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    lettersUsed.push(userGuess); 
+        document.getElementById("userguess-text").innerHTML = lettersUsed.join(", ");
     console.log(computerGuess);
     console.log(userGuess);
     console.log("--"); //used to seperate guesses in log//
-// figuring out how to save keys pressed in lettersUsed array // 
-if (userGuess === computerGuess) {
-    victories++;
-} else if (userGuess !== computerGuess)
-
-
-
+// sorting logic out and getting display on page.  
+    if (userGuess === computerGuess) {
+        victories++;
+        document.getElementById("victory-text").innerHTML = victories;
+        reset();
+    } 
+    if (livesLeft === 0)
+        losses++;
+            document.getElementById("lose-text").innerHTML = losses;
+        reset();
+    console.log(victories);
+    console.log(losses);
+    console.log(lettersUsed)
 }
